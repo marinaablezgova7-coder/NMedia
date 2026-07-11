83e70520b39d4eca1df2346cee35741c9611039f
gorzashita <marinaablezgova7@gmail.com>
2026-07-11 10:32:57 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.nmedia"
+    namespace = "ru.netology.nmedia"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
