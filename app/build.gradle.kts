6015ad17a957f10bbf482a7e5f0c373f3beb34e4
gorzashita <marinaablezgova7@gmail.com>
2026-07-12 19:50:14 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "= "ru.netology.nme"
+        applicationId = "= "ru.netology.nmedia"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
